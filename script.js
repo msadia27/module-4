928b46b5bada9ec01aa89e490ea4ed064e9d3672
Madelane Sadia <madelanecsadia@gmail.com>
2022-07-07 23:13:18 -0400

    start of javascript

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-
+var 
 // WHEN I answer a question THEN I am presented with another question
+
+document.getElementById ("start").onclick = function () {
+    alert("you must choose an answer to move forward")
+}
+
+function check() {
+    var correctAnswer = "c";
+    var inputValue = document.getElementById("button").value;
+  
+    if(inputValue !== correctAnswer) {
+      return false;
+    };
+  };
 //WHEN I answer a question incorrectly THEN time is subtracted from the clock
